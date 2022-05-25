e8f213e6a476b44145693070c9218320576e6296
MireiaBS <barrufetsoler.mireia@gmail.com>
2022-05-25 11:17:22 +0200

    comit2

---
+++ src/components/styled-components/boton.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const Button1 = styled.button`
+    background: bisque;
+    border: 1em;
+    border-radius: 2em;
+    margin: 2em;
+    padding-left: 20px;
+    padding-right: 20px;
+
+    &:hover {
+        background: coral; 
+        transition: all .4s ease;   
+    }
+`
+export const Button2 = styled.button`
+    background: pink;
+    padding: 20px;
+    font-size: 1cm;
+    border: 1em;
+    border-radius: 2em;
+    margin: 2em;
+    
+
+    &:hover {
+        background: orangered; 
+        transition: all .4s ease;   
+    }
+`
+    
