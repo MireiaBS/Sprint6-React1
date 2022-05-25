f3f8ccc338227d5d1998b9d69a8bac6e3246815a
MireiaBS <barrufetsoler.mireia@gmail.com>
2022-05-25 11:15:21 +0200

    ex-1-a-3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import React from "react";
+import Escena from "./components/escena/escena";
+import { datos } from "./datos";
+import "./styles.css";
+import { Button1, Button2 } from "./components/styled-components/boton";
 
-import './styles.css';
+let fraseActual = 0;
+  function App() {
+  function Anterior() {
+    
+  }
+  function Seguent() {
+    const siguienteFrase = fraseActual+1;
+    console.log(datos[fraseActual].frase)
 
-function App() {
+  }
+
+  const fraseResult = datos.map((dato) => {
+    return (
+      <Button1>
+        <Escena frase={dato.frase} />
+      </Button1>
+    );
+  });
+
   return (
-    <div> 'Hi' </div>
+    <div className="mainDiv">
+      <Button2 onClick={Anterior}>Anterior</Button2>
+      <Button2 onClick={Seguent}>Següent</Button2>
+      <div className="contentDiv">{fraseResult}</div>
+    </div>
   );
 }
 
