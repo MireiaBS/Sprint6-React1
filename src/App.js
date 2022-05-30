5a0977b88a04da925235d59dd0beb88a74781292
MireiaBS <barrufetsoler.mireia@gmail.com>
2022-05-30 13:07:09 +0200

    ex-4.1

--- src/App.js
+++ src/App.js
@@ -3,26 +3,33 @@ import Escena from "./components/escena/escena";
 import { datos } from "./datos";
 import "./styles.css";
 import { Button1, Button2 } from "./components/styled-components/boton";
-import CambioFrase from "./components/escena/CambioFrase"
 import classNames from "classnames";
 import { useState } from "react";
 
 function App() {
+  const [mark, setMark] = useState(0);
 
-  const [mark,setMark] = useState(0);
-
-  const fraseResult = datos.map((dato,i) => {
+  const fraseResult = datos.map((dato, i) => {
     return (
-      <Button1 className = {classNames({active: i === mark})}>
-        <Escena frase={dato.frase} />        
-      </Button1>     
+      <Button1 className={classNames({ active: i === mark })}>
+        <Escena frase={dato.frase} />
+      </Button1>
     );
-
   });
 
+  const Anterior = () => {
+    setMark(mark - 1);
+  };
+
+  const Siguiente = () => {
+    setMark(mark + 1);
+  };
   return (
     <div className="mainDiv">
-      <CambioFrase setMark={setMark} mark={mark}/>
+      <Button2 onClick={Anterior}>Anterior
+      </Button2>
+      <Button2 onClick={Siguiente}>Siguiente</Button2>
+     
       <div className="contentDiv">{fraseResult}</div>
     </div>
   );
